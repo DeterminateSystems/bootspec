f5056eb807bf6556f0580d419f61a4ac67ddf55c
Raito Bezarius <masterancpp@gmail.com>
2022-12-21 00:41:42 +0100

    bootspec(chore): test null cases for extensions and specialisation

--- bootspec/src/generation.rs
+++ bootspec/src/generation.rs
@@ -109,6 +109,7 @@ mod tests {
     fn valid_v1_json_with_typed_extension() {
         let json = r#"{
     "v1": {
+        "system": "x86_64-linux",
         "init": "/nix/store/xxx-nixos-system-xxx/init",
         "initrd": "/nix/store/xxx-initrd-linux/initrd",
         "initrdSecrets": "/nix/store/xxx-append-secrets/bin/append-initrd-secrets",
@@ -169,6 +170,7 @@ mod tests {
     fn valid_v1_json_without_extension() {
         let json = r#"{
     "v1": {
+        "system": "x86_64-linux",
         "init": "/nix/store/xxx-nixos-system-xxx/init",
         "initrd": "/nix/store/xxx-initrd-linux/initrd",
         "initrdSecrets": "/nix/store/xxx-append-secrets/bin/append-initrd-secrets",
@@ -223,6 +225,65 @@ mod tests {
     }
 
     #[test]
+    fn valid_v1_json_with_null_extension() {
+        let json = r#"{
+    "v1": {
+        "system": "x86_64-linux",
+        "init": "/nix/store/xxx-nixos-system-xxx/init",
+        "initrd": "/nix/store/xxx-initrd-linux/initrd",
+        "initrdSecrets": "/nix/store/xxx-append-secrets/bin/append-initrd-secrets",
+        "kernel": "/nix/store/xxx-linux/bzImage",
+        "kernelParams": [
+            "amd_iommu=on",
+            "amd_iommu=pt",
+            "iommu=pt",
+            "kvm.ignore_msrs=1",
+            "kvm.report_ignored_msrs=0",
+            "udev.log_priority=3",
+            "systemd.unified_cgroup_hierarchy=1",
+            "loglevel=4"
+        ],
+        "label": "NixOS 21.11.20210810.dirty (Linux 5.15.30)",
+        "toplevel": "/nix/store/xxx-nixos-system-xxx",
+        "specialisation": {},
+        "extensions": null
+    }
+}"#;
+
+        let from_json: Generation = serde_json::from_str(&json).unwrap();
+        let Generation::V1(from_json) = from_json;
+
+        let expected = crate::v1::GenerationV1 {
+            system: String::from("x86_64-linux"),
+            label: String::from("NixOS 21.11.20210810.dirty (Linux 5.15.30)"),
+            kernel: PathBuf::from("/nix/store/xxx-linux/bzImage"),
+            kernel_params: vec![
+                "amd_iommu=on",
+                "amd_iommu=pt",
+                "iommu=pt",
+                "kvm.ignore_msrs=1",
+                "kvm.report_ignored_msrs=0",
+                "udev.log_priority=3",
+                "systemd.unified_cgroup_hierarchy=1",
+                "loglevel=4",
+            ]
+            .iter()
+            .map(ToString::to_string)
+            .collect(),
+            init: PathBuf::from("/nix/store/xxx-nixos-system-xxx/init"),
+            initrd: Some(PathBuf::from("/nix/store/xxx-initrd-linux/initrd")),
+            initrd_secrets: Some(PathBuf::from(
+                "/nix/store/xxx-append-secrets/bin/append-initrd-secrets",
+            )),
+            specialisation: HashMap::new(),
+            extensions: None,
+            toplevel: SystemConfigurationRoot(PathBuf::from("/nix/store/xxx-nixos-system-xxx")),
+        };
+
+        assert_eq!(from_json, expected);
+    }
+
+    #[test]
     fn valid_v1_json_without_initrd_and_specialisation() {
         let json = r#"{
     "v1": {
@@ -240,7 +301,7 @@ mod tests {
             "loglevel=4"
         ],
         "label": "NixOS 21.11.20210810.dirty (Linux 5.15.30)",
-        "toplevel": "/nix/store/xxx-nixos-system-xxx",
+        "toplevel": "/nix/store/xxx-nixos-system-xxx"
     }
 }"#;
 
@@ -273,6 +334,34 @@ mod tests {
         };
 
         assert_eq!(from_json, expected);
+    }
+
+    #[test]
+    fn invalid_v1_json_with_null_specialisation() {
+        let json = r#"{
+    "v1": {
+        "init": "/nix/store/xxx-nixos-system-xxx/init",
+        "initrd": "/nix/store/xxx-initrd-linux/initrd",
+        "initrdSecrets": "/nix/store/xxx-append-secrets/bin/append-initrd-secrets",
+        "kernel": "/nix/store/xxx-linux/bzImage",
+        "kernelParams": [
+            "amd_iommu=on",
+            "amd_iommu=pt",
+            "iommu=pt",
+            "kvm.ignore_msrs=1",
+            "kvm.report_ignored_msrs=0",
+            "udev.log_priority=3",
+            "systemd.unified_cgroup_hierarchy=1",
+            "loglevel=4"
+        ],
+        "label": "NixOS 21.11.20210810.dirty (Linux 5.15.30)",
+        "toplevel": "/nix/store/xxx-nixos-system-xxx",
+        "specialisation": null
+    }
+}"#;
+
+        let json_err = serde_json::from_str::<Generation>(&json).unwrap_err();
+        assert!(json_err.to_string().contains("expected a map"));
     }
 
     #[test]
